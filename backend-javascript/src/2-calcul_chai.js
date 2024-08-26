d47caec384bb1aa0f713ce1a8559b3aeb1a3e1ba
QAunmaskify <jasmineolofin@gmail.com>
2024-08-26 17:45:05 +0100

    Replace nodejs 'assert' whith 'chai.expect'

---
+++ backend-javascript/src/2-calcul_chai.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+export function calculateNumber(type, a, b) {
+  switch (type) {
+    case "SUM":
+      return Math.round(a) + Math.round(b);
+
+    case "SUBTRACT":
+      return Math.round(a) - Math.round(b);
+
+    case "DIVIDE":
+      return Math.round(b) === 0 ? "Error" : Math.round(a) / Math.round(b);
+  }
+}
